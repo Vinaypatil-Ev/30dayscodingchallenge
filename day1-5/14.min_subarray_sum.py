f406cff3cb49b139e26cfbd97f0fe4cf372efaf8
Vinaypatil-Ev <vinaypatil153@gmail.com>
2022-01-04 23:40:54 +0530

    04 January 2022

---
+++ day1-5/14.min_subarray_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_len = float("inf")
+        left = right = 0
+        n = len(nums)
+        sums = 0
+        while right < n:
+            sums += nums[right]
+            right += 1
+            while sums >= target and left < right:
+                min_len = min(min_len, right - left)
+                sums -= nums[left]
+                left += 1
+            
+        return 0 if min_len == float("inf") else min_len
